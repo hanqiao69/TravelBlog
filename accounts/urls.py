44448802e5b743d350d16cb4aa01875a81b46399
Ken Schiller <kenschiller@gmail.com>
2015-02-13 02:45:55 -0600

    Implement janky profile image uploads

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from views import home, profile, publicprofile, profileself, \
-    groups, group, calupdate, brand
+    groups, group, calupdate, brand, update_image
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -23,7 +23,8 @@ urlpatterns = patterns(
     url(r'^profile/me/$', profileself),
     url(r'^profile/me/groups$', groups),
     url(r'^group/(?P<group_id>[0-9]+)/$', group),
-    url(r'^profile/update/$', calupdate),
+    url(r'^profile/update/$', calupdate, name='calupdate'),
+    url(r'^profile/update/image$', update_image, name='update_image'),
     url(r'^brand/$', brand),
 )
 urlpatterns += staticfiles_urlpatterns()
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+import os
+
+from django import forms
 from django.shortcuts import redirect, render_to_response
 # from django.template.loader import get_template
 # from django.template import Context
@@ -85,6 +88,10 @@ def profileself(request):
     return render_to_response('profile.html', data, RequestContext(request))
 
 
+class UploadFileForm(forms.Form):
+    image_file = forms.FileField(label='Select a file')
+
+
 @login_required(login_url='/login')
 def calupdate(request):
     if request.POST.get('mybtn'):
@@ -104,15 +111,41 @@ def calupdate(request):
         new_group.members.add(request.user)
         new_group.save()
         return redirect('/group/' + str(new_group.id))
-    print request
     user = CustomUser.objects.get(username=request.user)
     print user
     user_profile = user.get_user_profile()
     print "hello"
+    form = UploadFileForm()
     data = {'profile_image': user_profile.profile_image_url(),
-            'profile': user_profile}
+            'profile': user_profile,
+            'form': form}
 
     return render_to_response('calupdate.html', data, RequestContext(request))
+
+
+def handle_uplaoded_file(user, file_):
+    user_name = str(user)
+    images_dir = 'static/user_images/'
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+    image_name = 'user_images/' + user_name
+    user_profile = CustomUser.objects.get(username=user).get_user_profile()
+    user_profile.profile_image = image_name
+    user_profile.save()
+    image_path = 'static/' + image_name
+    print 'let\'s write to', image_path
+    with open(image_path, 'wb+') as destination:
+        for chunk in file_.chunks():
+            destination.write(chunk)
+
+
+@login_required(login_url='/login')
+def update_image(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uplaoded_file(request.user, request.FILES['image_file'])
+    return calupdate(request)
 
 
 def compile_results(string1):
