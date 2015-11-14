a12b8358617c53e3954201912130f273e2ac8dfb
Nina Lu <ninajlu@gmail.com>
2015-11-13 17:29:36 -0800

    please

--- brandplug/wsgi.py
+++ brandplug/wsgi.py
@@ -12,8 +12,4 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'brandplug.settings'
-#application = get_wsgi_application()
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
